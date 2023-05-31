f83e5b1514b021b9f451b244784108dd8ea3ee1e
bintang2101092040 <114200477+bintang2101092040@users.noreply.github.com>
2023-05-31 11:36:27 +0700

    Add files via upload

---
+++ uts_pbo2_BINTANGRAHMADHANDEVIS/InformasiSaldoModel.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts_pbo2_BINTANGRAHMADHANDEVIS;
+import uts_pbo2_BINTANGRAHMADHANDEVIS.TransferModel;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class InformasiSaldoModel {
+    String saldo="81000000";
+    
+
+    public String getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(String saldo) {
+        this.saldo = saldo;
+    }
+    
+}
